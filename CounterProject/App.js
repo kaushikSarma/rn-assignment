a3ff89509eab6d0f69068ef70a5433d2fd9e7045
Kaushik Sarma <kaushik.sarma@flipkart.com>
2021-09-21 20:50:31 +0530

    add commenst

--- CounterProject/App.js
+++ CounterProject/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ColorList from './src/ColorList';
 // import ProductList from './src/ProductList';
 
+// To View color Tshirt list please uncomment <ProductList />
+// and comment <ColorList /.
 export default function App() {
   // return <ProductList />;
   return <ColorList />;
